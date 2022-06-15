50b2f064e242967524af30b371df98c4de4ac7ea
James Barr <James.Matthew.Barr@gmail.com>
2022-06-15 20:59:01 +0100

    Add dt handling and sprint optional

--- drone.js
+++ drone.js
@@ -3,29 +3,39 @@ const GRAVITY = [0, 9.81];
 class Drone {
   constructor() {
     this.pos = [100, 100];
-    this.velocity = [0, 20];
-    this.width = 30;
+    this.velocity = [5, -5];
+    this.width = 100;
     this.height = 30;
     this.mass = 0.4; //Kg
   }
 
   /** @param {CanvasRenderingContext2D} ctx */
-  update() {
-    this.#move();
-    this.spring.height = this.pos[1];
+  update(dt) {
+    this.#move(dt);
+
+    if (this.spring) {
+      this.spring.height = this.pos[1];
+    }
   }
 
-  #move() {
-    const springForce = this.spring.calculateForce();
+  #move(dt) {
+    if (isNaN(dt)) return;
+
+    let springForce = [0, 0];
+
+    if (this.spring) {
+      springForce = this.spring.calculateForce();
+    }
 
     for (let dim = 0; dim < 2; dim++) {
-      const force = GRAVITY[dim] * this.mass - springForce[dim];
-      this.velocity[dim] += force * 0.01;
+      let force = GRAVITY[dim] * this.mass - springForce[dim];
+
+      this.velocity[dim] += force * 0.001 * dt;
       this.pos[dim] = this.pos[dim] + this.velocity[dim];
     }
 
-    if (this.pos[1] > 500 - this.height / 2) {
-      this.pos[1] = 500 - this.height / 2;
+    if (this.pos[1] > 500 - this.height) {
+      this.pos[1] = 500 - this.height;
       this.velocity = [0, 0];
     }
   }
@@ -35,13 +45,34 @@ class Drone {
     ctx.save();
 
     ctx.beginPath();
-    ctx.rect(
-      this.pos[0] - this.width / 2,
-      this.pos[1] - this.height / 2,
-      this.width,
-      this.height
-    );
+    ctx.translate(this.pos[0], this.pos[1]);
+    ctx.moveTo(0, 0);
+    ctx.lineTo(this.width / 2, this.height);
+    ctx.lineTo(-this.width / 2, this.height);
+    ctx.lineTo(0, 0);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(0, 15, 15, Math.PI, -Math.PI, true);
     ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(0, 15, 5, Math.PI, -Math.PI, true);
+    ctx.fillStyle = "red";
+    ctx.fill();
+
+    for (let index = 0; index < 2; index++) {
+      const side = index % 2 == 0;
+      ctx.beginPath();
+      ctx.rect(
+        ((this.width - 10) * (side ? 1 : -1)) / 2,
+        this.height / 2 + 4,
+        side ? 7 : -7,
+        this.height / 2
+      );
+      ctx.fillStyle = "black";
+      ctx.fill();
+    }
 
     ctx.restore();
   }
--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 /** @type {HTMLCanvasElement} */
 const droneCanvas = document.getElementById("droneCanvas");
-droneCanvas.width = 500;
+droneCanvas.width = 1500;
 droneCanvas.height = 500;
 
 const droneCtx = droneCanvas.getContext("2d");
 
 const drone = new Drone();
-const spring = new Spring(drone);
+// const spring = new Spring(drone);
+const spring = null;
+
+let perviousTime = 0;
 
 animate();
 
 function animate(time) {
+  const dt = time - perviousTime;
+  perviousTime = time;
   droneCtx.clearRect(0, 0, droneCanvas.width, droneCanvas.height);
 
-  drone.update();
+  drone.update(dt);
   drone.draw(droneCtx);
-  spring.draw(droneCtx);
+
+  if (spring) {
+    spring.draw(droneCtx);
+  }
+
   requestAnimationFrame(animate);
 }
