acd0bca2cc88114be5dc6717ddcd0425c59588b1
James Barr <James.Matthew.Barr@gmail.com>
2022-06-28 20:03:58 +0100

    Tidy brain.js

--- brain.js
+++ brain.js
@@ -1,124 +1,48 @@
 class Brain {
   constructor() {
+    // CONSTS
     this.numberOfInputs = 6;
     this.numberOfOutputs = 2;
     this.numberOfNodesInHiddenLayers = [8];
-    this.numberOfHiddenLayers = this.numberOfNodesInHiddenLayers.length;
+    this.mutationRate = 0.01;
+    this.randomValueLimit = 2;
 
+    // NEURAL NETWORK
     this.weights = [];
     this.bias = [];
     this.nodeValues = [];
-
-    this.mutationRate = 0.01;
 
     this.#initialiseWeightsAndBias();
   }
 
   /**
-   * Initalise the weights and bias to zero.
+   * Calculate the applied motor throttle from the neural network. The output
+   * is normalized between 0-1.
+   * @param {numbers[]} inputs - input from the drone as x-position, y-position,
+   * x-velocity, y-velocity, theta and omega.
+   * @returns {numbers[]} motor throttle as left then right motor
+   * @throws if the input array is not the expected length
    */
-  #initialiseWeightsAndBias() {
-    // loop over all the layers
-    for (let layer = 0; layer < this.numberOfHiddenLayers + 1; layer++) {
-      const layerWeights = [];
-      const numberOfNodesInPreviousLayer =
-        layer == 0
-          ? this.numberOfInputs
-          : this.numberOfNodesInHiddenLayers[layer - 1];
-
-      const numberOfInCurrentLayer =
-        layer == this.numberOfNodesInHiddenLayers.length
-          ? this.numberOfOutputs
-          : this.numberOfNodesInHiddenLayers[layer];
-
-      // loop over all the nodes in the layer
-      for (let node = 0; node < numberOfInCurrentLayer; node++) {
-        layerWeights.push(
-          this.#generateRandomWeightAndBias(numberOfNodesInPreviousLayer)
-        );
-      }
-
-      this.nodeValues.push(
-        this.#generateRandomWeightAndBias(numberOfInCurrentLayer)
-      );
-      this.bias.push(this.#generateRandomWeightAndBias(numberOfInCurrentLayer));
-
-      this.weights.push(layerWeights);
-    }
-  }
-
-  #generateRandomWeightAndBias(length) {
-    return this.#generateRandomArray(length, -2, 2);
-  }
-
-  #generateRandomArray(length, min, max) {
-    return Array.from({ length }, () => Math.random() * (max - min) + min);
-  }
-
-  /**
-   * Calculates the output of the neural network given the inputs.
-   * @param {Array<number>} inputs
-   */
-  #feedForward(inputs) {
+  calculateThrottle(inputs) {
     if (inputs.length !== this.numberOfInputs) {
       throw Error(`Number of inputs must be ${this.numberOfInputs}`);
     }
 
-    // loop over all the layers
-    for (let layer = 0; layer < this.numberOfHiddenLayers + 1; layer++) {
-      const numberOfNodesInPreviousLayer =
-        layer == 0
-          ? this.numberOfInputs
-          : this.numberOfNodesInHiddenLayers[layer - 1];
-
-      const numberOfInCurrentLayer =
-        layer == this.numberOfHiddenLayers
-          ? this.numberOfOutputs
-          : this.numberOfNodesInHiddenLayers[layer];
-
-      const previousLayerValues =
-        layer == 0 ? inputs : this.nodeValues[layer - 1];
-      const layerWeights = this.weights[layer];
-      const layerBias = this.bias[layer];
-
-      // loop over all the nodes in the layer
-      for (let node = 0; node < numberOfInCurrentLayer; node++) {
-        const nodeWeights = layerWeights[node];
-        let nodeValue = layerBias[node];
-
-        for (
-          let inputNode = 0;
-          inputNode < numberOfNodesInPreviousLayer;
-          inputNode++
-        ) {
-          nodeValue += nodeWeights[inputNode] * previousLayerValues[inputNode];
-        }
-
-        this.nodeValues[layer][node] = this.#activation(nodeValue);
-      }
-    }
-
-    return this.nodeValues[this.weights.length - 1];
-  }
-
-  #activation(z) {
-    return Math.tanh(z);
-  }
-
-  calculateThrottle(inputs) {
     const outputs = this.#feedForward(inputs);
     // normalise output between 0-1
     return [(outputs[0] + 1) / 2, (outputs[1] + 1) / 2];
   }
 
   /**
-   * Mate two brains and randomly select the weights and bias from either parent.
-   * @param {Brain} partner
+   * Mate two brains and randomly select the weights and bias from one of the
+   * parent.
+   * @param {Brain} partner - other brain to "mate" with
+   * @returns {Brain} offspring of the two parent brains
    */
   mate(partner) {
     const child = new Brain();
 
-    for (let layer = 0; layer < this.numberOfHiddenLayers + 1; layer++) {
+    for (let layer = 0; layer < this.#numberOfHiddenLayers + 1; layer++) {
       const numberOfNodesInPreviousLayer =
         layer == 0
           ? this.numberOfInputs
@@ -161,7 +85,7 @@ class Brain {
    * offsets is controlled using this.mutationRate.
    */
   mutate() {
-    for (let layer = 0; layer < this.numberOfHiddenLayers + 1; layer++) {
+    for (let layer = 0; layer < this.#numberOfHiddenLayers + 1; layer++) {
       const numberOfNodesInPreviousLayer =
         layer == 0
           ? this.numberOfInputs
@@ -186,5 +110,118 @@ class Brain {
         this.bias[layer][node] += (Math.random() * 2 - 1) * this.mutationRate;
       }
     }
+  }
+
+  /**
+   * Activation function of the node.
+   * @param {number} z - node value
+   * @returns {number} output from the node
+   */
+  #activation(z) {
+    return Math.tanh(z);
+  }
+
+  /**
+   * Calculates the output of the neural network given the inputs.
+   * @param {number[]} inputs - values from the drone
+   */
+  #feedForward(inputs) {
+    // loop over all the layers
+    for (let layer = 0; layer < this.#numberOfHiddenLayers + 1; layer++) {
+      const numberOfNodesInPreviousLayer =
+        layer == 0
+          ? this.numberOfInputs
+          : this.numberOfNodesInHiddenLayers[layer - 1];
+
+      const numberOfInCurrentLayer =
+        layer == this.#numberOfHiddenLayers
+          ? this.numberOfOutputs
+          : this.numberOfNodesInHiddenLayers[layer];
+
+      const previousLayerValues =
+        layer == 0 ? inputs : this.nodeValues[layer - 1];
+      const layerWeights = this.weights[layer];
+      const layerBias = this.bias[layer];
+
+      // loop over all the nodes in the layer
+      for (let node = 0; node < numberOfInCurrentLayer; node++) {
+        const nodeWeights = layerWeights[node];
+        let nodeValue = layerBias[node];
+
+        for (
+          let inputNode = 0;
+          inputNode < numberOfNodesInPreviousLayer;
+          inputNode++
+        ) {
+          nodeValue += nodeWeights[inputNode] * previousLayerValues[inputNode];
+        }
+
+        this.nodeValues[layer][node] = this.#activation(nodeValue);
+      }
+    }
+
+    return this.nodeValues[this.weights.length - 1];
+  }
+
+  /**
+   * Generates an array of given length populated with random numbers between
+   * -/+ @see Brain.randomValueLimit.
+   * @param {number} length - length of array
+   * @returns {number[]} array of random numbers
+   */
+  #generateRandomWeightAndBias(length) {
+    return this.#generateRandomArray(
+      length,
+      -this.randomValueLimit,
+      this.randomValueLimit
+    );
+  }
+
+  /**
+   * Generates an array of given length populated with random numbers between
+   * the min and max.
+   * @param {number} length - length of array
+   * @param {number} min - minimum value of number in array
+   * @param {number} max - maximum value of number in array
+   * @returns {number[]} array of random numbers
+   */
+  #generateRandomArray(length, min, max) {
+    return Array.from({ length }, () => Math.random() * (max - min) + min);
+  }
+
+  /**
+   * Initalises the weights and bias to a random values given by
+   * @see Brain.#generateRandomWeightAndBias.
+   */
+  #initialiseWeightsAndBias() {
+    // loop over all the layers
+    for (let layer = 0; layer < this.#numberOfHiddenLayers + 1; layer++) {
+      const layerWeights = [];
+      const numberOfNodesInPreviousLayer =
+        layer === 0
+          ? this.numberOfInputs
+          : this.numberOfNodesInHiddenLayers[layer - 1];
+
+      const numberOfInCurrentLayer =
+        layer === this.numberOfNodesInHiddenLayers.length
+          ? this.numberOfOutputs
+          : this.numberOfNodesInHiddenLayers[layer];
+
+      // loop over all the nodes in the layer
+      for (let node = 0; node < numberOfInCurrentLayer; node++) {
+        layerWeights.push(
+          this.#generateRandomWeightAndBias(numberOfNodesInPreviousLayer)
+        );
+      }
+
+      // set initial node values to 0
+      this.nodeValues.push(new Array(numberOfInCurrentLayer).fill(0));
+      this.bias.push(this.#generateRandomWeightAndBias(numberOfInCurrentLayer));
+      this.weights.push(layerWeights);
+    }
+  }
+
+  get #numberOfHiddenLayers() {
+    return this.numberOfNodesInHiddenLayers.length;
   }
 }
